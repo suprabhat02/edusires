fa15a556adbe53e2fe44a0ed5289ceda2a218b99
suprabhat02 <suprabhatkumar02@gmail.com>
2024-07-30 23:34:51 +0530

    added initial theme and functinlaity

---
+++ js/main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const themeToggleBtn = document.querySelector("#theme-toggle-btn");
+
+  // Check saved theme in localStorage
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+    document.body.classList.add(savedTheme);
+  } else {
+    // Default theme
+    document.body.classList.add("light-theme");
+  }
+
+  themeToggleBtn.addEventListener("click", function () {
+    if (document.body.classList.contains("light-theme")) {
+      document.body.classList.remove("light-theme");
+      document.body.classList.add("dark-theme");
+      localStorage.setItem("theme", "dark-theme");
+    } else {
+      document.body.classList.remove("dark-theme");
+      document.body.classList.add("light-theme");
+      localStorage.setItem("theme", "light-theme");
+    }
+  });
+});
